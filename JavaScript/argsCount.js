2d81bd4a2d620a6a3d0fa712716dcc7b447661fe
Jenny Le <jenny.le@ucalgary.ca>
2019-06-06 22:02:35 -0600

    Create argsCount.js

---
+++ JavaScript/argsCount.js
@@ -0,0 +1,13 @@
+/*
+How many arguments
+Level: 7 kyu
+Count the number of arguments
+
+Examples:
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+function args_count(){
+  return arguments.length;
+}

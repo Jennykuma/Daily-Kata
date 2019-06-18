96057b0edb052786f08ab150c9b9036a5705d24b
Jenny Le <jenny.le@ucalgary.ca>
2019-06-18 15:43:24 -0600

    Create generateShape.py

---
+++ Python/generateShape.py
@@ -0,0 +1,21 @@
+'''
+Build a square
+Level: 7 kyu
+I will give you an integer. Give me back a shape that is as long and wide as the integer. 
+The integer will be a whole number between 0 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+'''
+
+def generateShape(num):
+    result = ""
+    for x in range(num):
+      for y in range(num):
+          result = result + "+"
+      result = result + "\n"
+    return result[:-1]

1ea0a6a085fb758329b55fe8f35a56a057be0302
Jenny Le <jenny.le@ucalgary.ca>
2019-07-15 13:51:14 -0600

    Create goals.js

---
+++ JavaScript/goals.js
@@ -0,0 +1,19 @@
+/*
+Grasshopper - Messi goals function
+Level: 8 kyu
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions.
+Complete the goals function to return his total goals (the sum) for all three leagues.
+
+Note: the parameter for this function will always be a valid number.
+
+Ex :
+goals(5,10,2) == 5+10+2 = 17
+*/
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}

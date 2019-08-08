d75423990f464a43da869221b603d33dbda929fe
Jenny Le <jenny.le@ucalgary.ca>
2019-08-08 13:53:39 -0600

    Update countSheep.js

--- JavaScript/countSheep.js
+++ JavaScript/countSheep.js
@@ -1,7 +1,6 @@
 /*
 If you can't sleep, just count sheep!!
 Level: 8 kyu
-
 Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". 
 Input will always be valid, i.e. no negative integers.
 */

9881e65fefa8b6d17aba2732a1f42dcb4292883f
Jenny Le <jenny.le@ucalgary.ca>
2019-08-14 15:34:02 -0600

    Create stringToArray.js

---
+++ JavaScript/stringToArray.js
@@ -0,0 +1,12 @@
+/*
+Convert a string to an array
+Level: 8 kyu
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
+
+function stringToArray(string){
+  return string.split(" ").map((x) => x);
+}

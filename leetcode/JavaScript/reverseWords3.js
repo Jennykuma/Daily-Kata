f80f23cb5fa4ef782684a760bb004cca96fe3251
Jenny Le <jennyle.dev@gmail.com>
2019-10-22 19:39:21 -0600

    Create reverseWords3.js

---
+++ leetcode/JavaScript/reverseWords3.js
@@ -0,0 +1,23 @@
+/*
+Reverse Words in a String III
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving 
+whitespace and initial word order.
+
+Example 1:
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Note: In the string, each word is separated by single space and there will not be any extra space in the string.
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    var sArray = s.split(" ");
+    for(let i = 0; i < sArray.length; i++) {
+        sArray[i] = sArray[i].split('').reverse().join('');
+    }
+    return sArray.join(" ");
+};

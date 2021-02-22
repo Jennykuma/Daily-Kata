af52587b1446755172f009d2b47337ecf78a3eb7
Jenny <jenny.le@ucalgary.ca>
2021-02-21 21:27:02 -0700

    Add getAverage.js

---
+++ codewars/JavaScript/getAverage.js
@@ -0,0 +1,18 @@
+/*
+Get the mean of an array
+Level: 8 kyu
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+Examples:
+Test.assertEquals(getAverage([2,2,2,2]),2);
+Test.assertEquals(getAverage([1,2,3,4,5,]),3);
+Test.assertEquals(getAverage([1,1,1,1,1,1,1,2]),1);
+*/
+
+function getAverage(marks){
+  return Math.floor(marks.reduce((accumulated, currentValue) => accumulated + currentValue) / marks.length)
+}

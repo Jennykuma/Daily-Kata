5270627103b1fb783672063e330dfc9df830fcb8
Jenny <jenny.le@ucalgary.ca>
2021-03-16 20:25:04 -0600

    Add repeatStr.js

---
+++ codewars/JavaScript/repeatStr.js
@@ -0,0 +1,13 @@
+/*
+String repeat
+Level: 8 kyu
+
+Write a function called repeat_string which repeats the given string str exactly count times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
